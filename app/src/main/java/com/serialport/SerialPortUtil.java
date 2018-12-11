bb45c09b512c501fa2a073b04a139bb959bea3f1
liuhaijian <13621015367@136.com>
2018-12-11 21:55:19 +0800

    修改文件名，增加打开和关闭串口通讯的方法

--- app/src/main/java/com/serialport/SerialPortUtil.java
+++ app/src/main/java/com/serialport/SerialPortUtil.java
@@ -1,9 +1,55 @@
 package com.serialport;
 
+import android.util.Log;
+
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  * 创建人： 刘海剑
  * 创建时间： on 2018/12/11 0011.21:30
  * 功能描述：
  */
 public class SerialPortUtil {
+
+    private static final String TAG = "SerialPort";
+
+    /*
+     * Do not remove or rename the field mFd: it is used by native method close();
+     */
+    private FileDescriptor mFd;
+    private FileInputStream mFileInputStream;
+    private FileOutputStream mFileOutputStream;
+
+    public SerialPortUtil(String device, int baudrate, int flags) throws SecurityException, IOException {
+
+        /* Check access permission */
+        mFd = open(device, baudrate, flags);
+        if (mFd == null) {
+            Log.e(TAG, "native open returns null");
+            throw new IOException();
+        }
+        mFileInputStream = new FileInputStream(mFd);
+        mFileOutputStream = new FileOutputStream(mFd);
+    }
+
+    // Getters and setters
+    public InputStream getInputStream() {
+        return mFileInputStream;
+    }
+
+    public OutputStream getOutputStream() {
+        return mFileOutputStream;
+    }
+
+    // JNI
+    private native static FileDescriptor open(String path, int baudrate, int flags);
+    public native void close();
+    static {
+        System.loadLibrary("serialport");
+    }
 }
